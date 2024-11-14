717496c6e3e751dbe82ddb7ff3aef55fb5325d34
Esat <147237700+E5AT@users.noreply.github.com>
2024-11-14 11:28:13 +0200

    Created FormsApp6

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter w = new StreamWriter("textFile.txt", false);
+            w.WriteLine(richTextBox1.Text);
+            w.Close();
+        }
+    }
+}

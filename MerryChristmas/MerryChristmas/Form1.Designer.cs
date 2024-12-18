062e3b276733567fe3f35bdff0c8afc1cdb3a1c6
Esat <147237700+E5AT@users.noreply.github.com>
2024-12-18 13:56:38 +0200

    i fcd up

---
+++ MerryChristmas/MerryChristmas/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MerryChristmas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            richTextBox2 = new RichTextBox();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            richTextBox1.Location = new Point(161, 24);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(792, 346);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(11, 37);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(54, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Тип 1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(11, 62);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(54, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Тип 2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(11, 87);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(54, 19);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Тип 3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(11, 112);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(54, 19);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Тип 4";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(11, 137);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(54, 19);
+            radioButton5.TabIndex = 6;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Тип 5";
+            radioButton5.UseVisualStyleBackColor = true;
+            radioButton5.CheckedChanged += radioButton5_CheckedChanged;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(209, 391);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(298, 212);
+            richTextBox2.TabIndex = 7;
+            richTextBox2.Text = "";
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(12, 22);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(171, 19);
+            radioButton6.TabIndex = 8;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Аз искам да въведа имена!";
+            radioButton6.UseVisualStyleBackColor = true;
+            radioButton6.CheckedChanged += radioButton6_CheckedChanged;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(10, 50);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(184, 19);
+            radioButton7.TabIndex = 9;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "Имам текстов файл с имена!";
+            radioButton7.UseVisualStyleBackColor = true;
+            radioButton7.CheckedChanged += radioButton7_CheckedChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(13, 42);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(142, 170);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Моля изберете типа на съобщението:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton7);
+            groupBox2.Controls.Add(radioButton6);
+            groupBox2.Location = new Point(7, 363);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(196, 75);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Имена:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(209, 373);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 12;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(536, 391);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 84);
+            button1.TabIndex = 1;
+            button1.Text = "Генерирай";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 596);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(richTextBox2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private RichTextBox richTextBox2;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ MerryChristmas/MerryChristmas/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using System.Globalization;
+
+namespace MerryChristmas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public StreamReader r;
+        public string style;
+        public string[] names;
+        string dateTime = DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!radioButton1.Checked && !radioButton2.Checked && !radioButton3.Checked && !radioButton4.Checked && !radioButton5.Checked)
+                MessageBox.Show("Моля изберете тип!");
+
+            else if (!radioButton6.Checked && !radioButton7.Checked)
+                MessageBox.Show("Моля изберете имената!");
+
+            else if (richTextBox2.Text == string.Empty)
+                MessageBox.Show("Моля въведете имената!");
+            
+            else if (radioButton6.Checked)
+            {
+                Directory.CreateDirectory($"messages\\{dateTime}");
+                names = richTextBox2.Lines;
+                foreach(string name in names)
+                {
+                    StreamWriter w = new StreamWriter($"messages\\{dateTime}\\{name}.txt");
+                    w.Write($"{style.Replace("{name}",name)}");
+                    w.Close();
+                }
+                MessageBox.Show($"Съобщенията са генерирани и са запазени на: C:\\Users\\КрумМанолов\\source\\repos\\MerryChristmas\\MerryChristmas\\bin\\Debug\\net8.0-windows\\messages\\{dateTime}");
+
+            }
+            else if (radioButton7.Checked)
+            {
+                
+            }
+        }
+
+        public void previewChange(int num)
+        {
+            r = new StreamReader($"types\\{num}.txt");
+            style = r.ReadToEnd();
+            richTextBox1.Text = style;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            previewChange(1);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            previewChange(2);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            previewChange(3);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            previewChange(4);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            previewChange(5);
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Моля въведете имената:";
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Моля, въведете пътят до текстоия файл";
+        }
+    }
+}
